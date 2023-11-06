e0a0b044d5da65894a588dd787ed1e7605a3be41
analunalu <148767654+analunalu@users.noreply.github.com>
2023-11-06 08:27:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-function.tocaSom (idElementAudio) {
-    querySelector(idElementAudio)
+function tocaSom (idElementAudio) {
+   document.querySelector(idElementAudio).play()
 }
+
 const listadeTeclas = document.querySelectorAll ('.tecla');
+
 let contador = 0;
 
-while (contador < listaDeTeclas.lenght);
-listaDeTeclas[contador].onclick = tocaSom; {
-    contador = contador+1;
+for (let contador = 0; contador < listaDeTeclas.lenght; contador++) {
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classlist[1];
+    const idAudio = `#som_${instrumento}`; //tamplate string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
 }
+
+  
